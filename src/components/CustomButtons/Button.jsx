1f31ad9dda0cdbf3ded778cc2b7e40b054ada7ad
dansagam <olukayodeogunnowo@yahoo.com>
2021-12-07 14:25:40 +0100

    finish the button component

--- src/components/CustomButtons/Button.jsx
+++ src/components/CustomButtons/Button.jsx
@@ -1,7 +1,126 @@
-import React from 'react'
+import MuiButton from '@mui/material/Button'
+// eslint-disable-next-line no-unused-vars
+import { experimentalStyled as styled, makeStyles } from '@mui/material/styles'
+import PropTypes from 'prop-types'
+import styles from './buttonStyles'
 
-const Button = function () {
-   return <div>sas</div>
+// const useStyles = makeStyles(styles)
+
+const ButtonRoot = styled(MuiButton, {
+   shouldForwardProp: (prop) => prop !== 'size' && prop !== 'color',
+})(
+   ({
+      // eslint-disable-next-line no-unused-vars
+      theme,
+      size,
+      color,
+      round,
+      disabled,
+      simple,
+      block,
+      link,
+      justIcon,
+   }) => ({
+      ...(size && styles[size]),
+      ...(color && styles[color]),
+      ...(round && styles.round),
+      ...(disabled && styles.disabled),
+      ...(simple && styles.simple),
+      ...(block && styles.block),
+      ...(link && styles.link),
+      ...styles.button,
+      ...(justIcon && styles.justIcon),
+      // borderRadius: 0,
+      // fontWeight: theme.typography.fontWeightMedium,
+      // fontFamily: theme.typography.h1.fontFamily,
+      // padding: theme.spacing(2, 4),
+      // fontSize: theme.typography.pxToRem(14),
+      // boxShadow: 'none',
+      // '&:active, &:focus': {
+      //    boxShadow: 'none',
+      // },
+      // ...(size === 'small' && {
+      //    padding: theme.spacing(1, 3),
+      //    fontSize: theme.typography.pxToRem(13),
+      // }),
+      // ...(size === 'large' && {
+      //    padding: theme.spacing(2, 5),
+      //    fontSize: theme.typography.pxToRem(16),
+      // }),
+   })
+)
+
+const RegularButton = () => {
+   const {
+      color,
+      round,
+      children,
+      disabled,
+      simple,
+      size,
+      block,
+      link,
+      justIcon,
+      className,
+      muiClasses,
+      ...rest
+   } = props
+   // const classes = useStyles()
+   // const btnClasses = classNames({
+   //    [classes.button]: true,
+   //    [classes[size]]: size,
+   //    [classes[color]]: color,
+   //    [classes.round]: round,
+   //    [classes.disabled]: disabled,
+   //    [classes.simple]: simple,
+   //    [classes.block]: block,
+   //    [classes.link]: link,
+   //    [classes.justIcon]: justIcon,
+   //    [className]: className,
+   // })
+   return (
+      <ButtonRoot
+         block={block}
+         color={color}
+         disabled={disabled}
+         simple={simple}
+         link={link}
+         size={size}
+         round={round}
+         classes={muiClasses}
+         justIcon={justIcon}
+         {...rest}
+      >
+         {children}
+      </ButtonRoot>
+      // <MuiButton {...rest} classes={muiClasses} className={btnClasses}>
+      //    {children}
+      // </MuiButton>
+   )
 }
 
-export default Button
+RegularButton.propTypes = {
+   color: PropTypes.oneOf([
+      'primary',
+      'info',
+      'success',
+      'warning',
+      'danger',
+      'rose',
+      'white',
+      'transparent',
+   ]),
+   size: PropTypes.oneOf(['small', 'large']),
+   simple: PropTypes.bool,
+   round: PropTypes.bool,
+   disabled: PropTypes.bool,
+   block: PropTypes.bool,
+   link: PropTypes.bool,
+   justIcon: PropTypes.bool,
+   className: PropTypes.string,
+   // use this to pass the classes props from Material-UI
+   muiClasses: PropTypes.object,
+   children: PropTypes.node,
+}
+
+export default RegularButton
--- src/components/CustomInputs/Input.jsx
+++ src/components/CustomInputs/Input.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Input from '@mui/material/Input'
+import Clear from '@mui/icons-material/Clear'
+import Check from '@mui/icons-material/Check'
 
-const Input = function () {
-   return <div>dsdjhds</div>
+const CustomInput = (props) => {
+   const {
+      formControlProps,
+      labelText,
+      id,
+      labelProps,
+      inputProps,
+      error,
+      success,
+      // eslint-disable-next-line no-unused-vars
+      rtlActive,
+   } = props
+
+   const newInputProps = {
+      maxLength:
+         inputProps && inputProps.maxLength ? inputProps.maxLength : undefined,
+      minLength:
+         inputProps && inputProps.minLength ? inputProps.minLength : undefined,
+      step: inputProps && inputProps.step ? inputProps.step : undefined,
+   }
+   return (
+      <FormControl {...formControlProps} className="#">
+         {labelText !== undefined ? (
+            <InputLabel className="ddh" htmlFor={id} {...labelProps}>
+               {labelText}
+            </InputLabel>
+         ) : null}
+         <Input
+            className="hjdkd"
+            id={id}
+            {...inputProps}
+            inputProps={newInputProps}
+         />
+         {error ? <Clear /> : success ? <Check /> : null}
+      </FormControl>
+   )
 }
 
-export default Input
+CustomInput.propTypes = {
+   labelText: PropTypes.node,
+   labelProps: PropTypes.object,
+   id: PropTypes.string,
+   inputProps: PropTypes.object,
+   formControlProps: PropTypes.object,
+   error: PropTypes.bool,
+   success: PropTypes.bool,
+   rtlActive: PropTypes.bool,
+}
+
+export default CustomInput
