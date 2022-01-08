442f1fdfb8a8ab2cabf92d5fcf2eb61033ce2998
dansagam <olukayodeogunnowo@yahoo.com>
2022-01-08 11:40:32 +0100

    set up the server connection to mongoDB

---
+++ server/db/connectDB.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+   try {
+      mongoose.Promise = global.Promise
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         // useFindAndModify: false,
+         // useCreateIndex: true,
+      })
+      // console.log(conn)
+      console.log(`💻 ${conn.connection.host} CONNECTED to the DB successfully`)
+   } catch (err) {
+      console.log(`Error ${err.message}`)
+      process.exit(1)
+   }
+}
+
+export default connectDB
